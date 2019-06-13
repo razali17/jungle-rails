67360513fc9caf3f63a7ecbd01d640e136acfc37
vagrant <r.ali@mail.utoronto.ca>
2019-06-13 13:30:08 +0000

    implemented mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def order_receipt(user, orderID)
+    mail(to: user.email, subject: "Receipt for #{orderID}")
+  end
+end
